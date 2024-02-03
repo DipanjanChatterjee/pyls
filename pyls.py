dd029e481c5f00620864360d1ab46d44abcb35b6
DipanjanChatterjee <48484901+DipanjanChatterjee@users.noreply.github.com>
2024-02-03 20:52:03 +0530

    Version - 0.6 Enhancement: pyls -l --filter=<option> (#8)
    Enhancement: pyls -l --filter command added, which filtered out file or directories based on provided options(file, dir). if file is provided then all files will be filtered out else if dir is provided then all dir will be filtered out else error message will be displayed.
    
    Files:
    Update: pyls.py (c functionality has been added with --filter Arguement Parser and method command_filter etc.).
    Add: pyls_-l_-filter=dir.png (Example image taken from the output)
    Add: pyls_-l_-filter=file.png (Example image taken from the output)
    Add: pyls_-l_-filter=folder.png (Example image taken from the output)
    Update: README.md (pyls -l --filter command details is added)

--- pyls.py
+++ pyls.py
@@ -1,3 +1,4 @@
+import re
 import json
 import argparse
 from datetime import datetime
@@ -21,8 +22,8 @@ class UnixCommand:
     def output_generated_command(self, a_flag: bool, l_flag: bool) -> str:
         """
         Generate Output for commands like ls, A, l
-        :param a_flag: boolean type if false then any files starts with '.', will be ignored else will be included
-        :param l_flag: type: boolean type if false then only file name will be going to shown else every details like
+        :param a_flag: Boolean type if false then any files starts with '.', will be ignored else will be included
+        :param l_flag: type: Boolean type if false then only file name will be going to shown else every details like
         permission, file or directory size, date, name vertically will be shown
         :return: type: string type as a final output
         """
@@ -47,14 +48,36 @@ class UnixCommand:
     # r - reverse the order of the file or directory
     def command_r(self):
         """
-        Reverse the order of the file or directory and update the existing the structure.
+        Reverse the order of the file or directory and update the existing structure.
         :return: None
         """
         self.structure = reversed(self.structure)
 
     # t - sorted the order of the file or directory based on time (oldest first)
     def command_t(self):
+        """
+        Sort the order based on time (oldest first) of the file or directory and update the existing structure.
+        :return: None
+        """
         self.structure = sorted(self.structure, key=lambda item: datetime.fromtimestamp(item.get('time_modified')))
+
+    # filter - filter out files or directory based on the filter type
+    def command_filter(self, filter_name: str):
+        """
+        Filter out files or directory.
+        :param filter_name: String type if file then all files will be filtered out else if directory then all
+        directories will be filtered out else error will be thrown with proper information and update the existing
+        structure.
+        :return: None
+        """
+        if filter_name.lower() == 'dir':
+            self.structure = [items for items in self.structure if
+                              len(re.findall('(?i)\\.|LICENSE', items.get('name'))) <= 0]
+        elif filter_name.lower() == 'file':
+            self.structure = [items for items in self.structure if
+                              len(re.findall('(?i)\\.|LICENSE', items.get('name'))) > 0]
+        else:
+            raise Exception(f"'{filter_name}' is not a valid filter criteria. Available filters are 'dir' and 'file'")
 
 
 # Argument Parser Inputs
@@ -71,15 +94,19 @@ parser.add_argument('-l', action='store_true',
 parser.add_argument('-r', action='store_true', help='Provide -r to get the files and directories in reverse order.')
 parser.add_argument('-t', action='store_true',
                     help='Provide -t to get the files and directories in sorted order based on time (oldest first).')
+parser.add_argument('--filter', type=str, default='',
+                    help='Provide --filter to filter out files or directories, available options are file and dir.')
 args = parser.parse_args()  # Creating the argument object to parse argument
 
-# Parsing Arguments
+# Parsing Arguments and Defined Variables
 structure_path = args.structure
 path = args.path
 all_file_flag = args.A
 list_info_flag = args.l
 reverse_flag = args.r
 time_sorting_flag = args.t
+filter_type = args.filter
+error_flag = False
 
 # Reading Structures
 file_object = open(structure_path, 'r')
@@ -92,10 +119,15 @@ if path:
     print()
 else:
     command_object.directory_parser('interpreter')
-
+if filter_type:
+    try:
+        command_object.command_filter(filter_name=filter_type)
+    except Exception as error_message:
+        error_flag = True
+        print(', '.join(error_message.args))
 if time_sorting_flag:
     command_object.command_t()
 if reverse_flag:
     command_object.command_r()
-
-print(command_object.output_generated_command(all_file_flag, list_info_flag))
+if not error_flag:
+    print(command_object.output_generated_command(all_file_flag, list_info_flag))
