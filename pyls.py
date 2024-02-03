c9b81f719624ad5eb66c26da48b2a24f4ea82898
DipanjanChatterjee <48484901+DipanjanChatterjee@users.noreply.github.com>
2024-02-03 13:23:55 +0530

    Version - 0.1 Enhancement: pyls (#1)
    Enhancement: Pyls command added, which lists out the top level (in the directory interpreter) directories and files, and it ignores the file which starts with '.'.
    
    Files:
    Add: pyls.py (It will execute the command).
    Add: pyls.png (Example image taken from the output)
    Add: Structure.json (It is getting used for parsing directory)
    Update: README.md (pyls command details is added)

---
+++ pyls.py
@@ -0,0 +1,45 @@
+import json
+import argparse
+
+
+class UnixCommand:
+
+    def __init__(self, structure_dict: dict):
+        self.structure = structure_dict
+
+    # Parse Directory
+    def directory_parser(self, name: str):
+        if self.structure.get('name') == name:
+            self.structure = self.structure.get('contents')
+
+    # Output Generated Commands
+    # ls - lists out the top level directories and files, and it ignores those files which starts with '.'
+    def ls(self) -> str:
+        return ' '.join([items.get('name') for items in self.structure if not items.get('name').startswith('.')])
+
+
+# Argument Parser Inputs
+parser = argparse.ArgumentParser(description='Unix Command Executor For Directory Parser')
+parser.add_argument('--structure', type=str, default='Structure/Structure.json',
+                    help='Provide Structure file location, Default Structure.json')
+parser.add_argument('path', nargs='?', type=str, default='',
+                    help='Provide file or directory name or relative path')
+args = parser.parse_args()  # Creating argument object to parse argument
+
+# Parsing Arguments
+structure_path = args.structure
+path = args.path
+
+# Reading Structures
+file_object = open(structure_path, 'r')
+structure = json.load(file_object)
+file_object.close()
+
+command_object = UnixCommand(structure)  # Creating Unix Command Objects
+
+if path:
+    print()
+else:
+    command_object.directory_parser('interpreter')
+
+print(command_object.ls())
