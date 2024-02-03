dac57330aa92ada77f3ff404161c6e11123bcde8
DipanjanChatterjee <dipchatterjee4040@gmail.com>
2024-02-03 18:10:22 +0530

    Version - 0.3 Enhancement: pyls -l
    Enhancement: pyls -l command added, which prints details information of the files and directories like permission, file or directory size, date, name.
    
    Files:
    Update: pyls.py (-l functionality has been added with -l Arguement Parser, but method command_a and command_ls removed instead created a generic method(output_generated_command) for handling ls, a, l.).
    Add: pyls_-l.png (Example image taken from the output)
    Update: README.md (pyls -l command details is added)

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 2. **`pyls -A`**: This prints all the files and directories (including files starting with '.'). Example:
     
     ![pyls -A](images/pyls_-A.png "pyls -A")
+3. **`pyls -l`**: This prints details information of the files and directories like permission, file or directory size, date, name. Example:
+
+   ![pyls -l](images/pyls_-l.png "pyls -l")
--- pyls.py
+++ pyls.py
@@ -1,5 +1,6 @@
 import json
 import argparse
+from datetime import datetime
 
 
 class UnixCommand:
@@ -13,14 +14,31 @@ class UnixCommand:
             self.structure = self.structure.get('contents')
 
     # Output Generated Commands
-
     # ls - lists out the top level directories and files, and it ignores those files which starts with '.'
-    def command_ls(self) -> str:
-        return ' '.join([items.get('name') for items in self.structure if not items.get('name').startswith('.')])
-
     # A - lists out the top level directories and files, and it ignores those files which starts with '.'
-    def command_a(self) -> str:
-        return ' '.join([items.get('name') for items in self.structure])
+    def output_generated_command(self, a_flag: bool, l_flag: bool) -> str:
+        """
+        Generate Output for commands like ls, A, l
+        :param a_flag: boolean type if false then any files starts with '.', will be ignored else will be included
+        :param l_flag: type: boolean type if false then only file name will be going to shown else every details like
+        permission, file or directory size, date, name will be shown
+        :return: type: string type as a final output
+        """
+        if l_flag and a_flag:  # pyls -l -A
+            return '\n'.join([
+                f'{items.get("permissions")} {items.get("size")} '
+                f'{datetime.fromtimestamp(items.get("time_modified")).strftime("%b %d %H:%M")} {items.get("name")}'
+                for items in self.structure
+            ])
+        elif l_flag and not a_flag:  # pyls -l
+            return '\n'.join([
+                f'{items.get("permissions")} {items.get("size")} '
+                f'{datetime.fromtimestamp(items.get("time_modified")).strftime("%b %d %H:%M")} {items.get("name")}'
+                for items in self.structure if not items.get('name').startswith('.')])
+        elif not l_flag and a_flag:  # pyls -A
+            return ' '.join([items.get('name') for items in self.structure])
+        else:  # pyls
+            return ' '.join([items.get('name') for items in self.structure if not items.get('name').startswith('.')])
 
 
 # Argument Parser Inputs
@@ -31,12 +49,16 @@ parser.add_argument('path', nargs='?', type=str, default='',
                     help='Provide file or directory name or relative path')
 parser.add_argument('-A', action='store_true',
                     help='Provide -A to get all the files and directories including files starting with "."')
-args = parser.parse_args()  # Creating argument object to parse argument
+parser.add_argument('-l', action='store_true',
+                    help='Provide -l to get all the files and directories information like permission, '
+                         'file or directory size, date, name.')
+args = parser.parse_args()  # Creating the argument object to parse argument
 
 # Parsing Arguments
 structure_path = args.structure
 path = args.path
 all_file_flag = args.A
+list_info_flag = args.l
 
 # Reading Structures
 file_object = open(structure_path, 'r')
@@ -50,7 +72,4 @@ if path:
 else:
     command_object.directory_parser('interpreter')
 
-if not all_file_flag:
-    print(command_object.command_ls())
-else:
-    print(command_object.command_a())
+print(command_object.output_generated_command(all_file_flag, list_info_flag))
