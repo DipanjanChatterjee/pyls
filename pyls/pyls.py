9ba7d3336cc87c0fecabea53914c51f9fd4b92ad
DipanjanChatterjee <48484901+DipanjanChatterjee@users.noreply.github.com>
2024-02-05 05:35:31 +0530

    Version - 1.0 Enhancement: Installable of pyls (#13)
    Enhancement: pyls installable has been created, which will require pip install <wheel_file_name>.
    
    Files:
    Update: pyls.py (Whole segment of argparse and other global section moved into function called execute and this file got moved into pyls folder).
    Add: .gitignore (Excluded __pycache__, pyls.egg-info folders)
    Add: dist/pyls-1.0.tar.gz (Installable Created for source file)
    Add: dist/pyls-1.0-py3-none-any.whl (Installable Created for wheel file)
    Add: pyproject.toml (Created for installable creation)
    Update: README.md (pyls all details has been added.)

--- pyls/pyls.py
+++ pyls/pyls.py
@@ -2,6 +2,7 @@ import re
 import sys
 import json
 import argparse
+from pathlib import Path
 from typing import Union
 from datetime import datetime
 
@@ -134,87 +135,91 @@ def handle_directory_path(directory_info: Union[dict, list], path_list: list, ro
                 yield result
 
 
-# Argument Parser Inputs
-parser = argparse.ArgumentParser(
-    prog='PYLS',
-    usage='%(prog)s retrieve and display the top-level directories and files from a JSON file, excluding those whose'
-          ' names start with "."',
-    description='This is equipped to execute a variety of commands, including Unix commands such as ls, '
-                'ls -A, ls -l, and more alongside some custom commands.\nExplore the extensive range of functionalities'
-                ' by referring to the provided arguments.',
-    epilog='''Example:
-    python pyls.py | pyls 
-    python pyls.py --structure=Structure/Structure.json  | pyls --structure=Structure/Structure.json
-    python pyls.py --root=interpreter | pyls --root=interpreter
-    python pyls.py  parser | pyls parser
-    python pyls.py  -A | pyls -A
-    python pyls.py  -l | pyls -l
-    python pyls.py  -l -r | pyls -l -r
-    python pyls.py  -l -t | pyls -l -t
-    python pyls.py  -l --filter=file | python pyls.py  -l --filter=dir | pyls -l --filter=file | pyls  -l --filter=dir
-    python pyls.py  -l -h | pyls -l -h
-    python pyls.py  --help | pyls --help
-    ''',
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    add_help=False)
-parser.add_argument('--structure', type=str, default='Structure/Structure.json',
-                    help='JSON file location which holds files and directory information. Default value is '
-                         'Structure/Structure.json')
-parser.add_argument('--root', type=str, default='interpreter',
-                    help='Decide the root directory. Default value is interpreter')
-parser.add_argument('path', nargs='?', type=str, default='',
-                    help='Handle paths to navigate the directory structure, also supports relative paths')
-parser.add_argument('-A', action='store_true',
-                    help='Show all the files and directories including files starting with "."')
-parser.add_argument('-l', action='store_true',
-                    help='Show the files and directories information like permission, size, date, name vertically.')
-parser.add_argument('-r', action='store_true', help='Reverse the order of files and directories.')
-parser.add_argument('-t', action='store_true', help='Sort files and directories based on time (oldest first).')
-parser.add_argument('--filter', type=str, default='',
-                    help='Filter out files or directories. Available options are 1. file 2. dir.')
-parser.add_argument('-h', action='store_true', help='Convert size of file or directories into human-readable size.')
-parser.add_argument('--help', action='help', default=argparse.SUPPRESS, help='Show help message and exit')
-args = parser.parse_args()  # Creating the argument object to parse argument
-
-# Parsing Arguments and Defined Variables
-structure_path = args.structure
-root_directory = args.root
-path = args.path
-all_file_flag = args.A
-list_info_flag = args.l
-reverse_flag = args.r
-time_sorting_flag = args.t
-filter_type = args.filter
-human_size_flag = args.h
 is_path_found = False
 
-# Reading Structures
-file_object = open(structure_path, 'r')
-structure = json.load(file_object)
-file_object.close()
 
-if path:
-    for updated_structure in handle_directory_path(structure, re.sub('^\\./', f'{root_directory}/', path).split('/'),
-                                                   ['.'] if '/' in path else []):
-        structure = updated_structure
-else:
-    for updated_structure in handle_directory_path(structure, root_directory.split('/')):
-        structure = updated_structure
+def execute():
+    # Argument Parser Inputs
+    parser = argparse.ArgumentParser(
+        prog='PYLS',
+        usage='%(prog)s retrieve and display the top-level directories and files from a JSON file, excluding those '
+              'whose names start with "."',
+        description='This is equipped to execute a variety of commands, including Unix commands such as ls, '
+                    'ls -A, ls -l, and more alongside some custom commands.\nExplore the extensive range of '
+                    'functionalities by referring to the provided arguments.',
+        epilog='''Example:
+        python pyls.py | pyls 
+        python pyls.py --structure=Structure/Structure.json  | pyls --structure=Structure/Structure.json
+        python pyls.py --root=interpreter | pyls --root=interpreter
+        python pyls.py  parser | pyls parser
+        python pyls.py  -A | pyls -A
+        python pyls.py  -l | pyls -l
+        python pyls.py  -l -r | pyls -l -r
+        python pyls.py  -l -t | pyls -l -t
+    python pyls.py  -l --filter=file | python pyls.py  -l --filter=dir | pyls -l --filter=file | pyls -l --filter=dir
+        python pyls.py  -l -h | pyls -l -h
+        python pyls.py  --help | pyls --help
+        ''',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False)
+    parser.add_argument('--structure', type=str, default='Structure/Structure.json',
+                        help='JSON file location which holds files and directory information. Default value is '
+                             'Structure/Structure.json')
+    parser.add_argument('--root', type=str, default='interpreter',
+                        help='Decide the root directory. Default value is interpreter')
+    parser.add_argument('path', nargs='?', type=str, default='',
+                        help='Handle paths to navigate the directory structure, also supports relative paths')
+    parser.add_argument('-A', action='store_true',
+                        help='Show all the files and directories including files starting with "."')
+    parser.add_argument('-l', action='store_true',
+                        help='Show the files and directories information like permission, size, date, name vertically.')
+    parser.add_argument('-r', action='store_true', help='Reverse the order of files and directories.')
+    parser.add_argument('-t', action='store_true', help='Sort files and directories based on time (oldest first).')
+    parser.add_argument('--filter', type=str, default='',
+                        help='Filter out files or directories. Available options are 1. file 2. dir.')
+    parser.add_argument('-h', action='store_true', help='Convert size of file or directories into human-readable size.')
+    parser.add_argument('--help', action='help', default=argparse.SUPPRESS, help='Show help message and exit')
+    args = parser.parse_args()  # Creating the argument object to parse argument
 
-if not is_path_found:
-    print(f"error: can not access '{path}': No such file or directory")
-    sys.exit()
+    # Parsing Arguments and Defined Variables
+    structure_path = args.structure
+    root_directory = args.root
+    path = args.path
+    all_file_flag = args.A
+    list_info_flag = args.l
+    reverse_flag = args.r
+    time_sorting_flag = args.t
+    filter_type = args.filter
+    human_size_flag = args.h
 
-command_object = UnixCommand(structure)  # Creating Unix Command Objects
+    # Reading Structures
+    file_object = open(Path(Path(__file__).parent, structure_path), 'r')
+    structure = json.load(file_object)
+    file_object.close()
 
-if filter_type:
-    try:
-        command_object.command_filter(filter_name=filter_type)
-    except Exception as error_message:
-        print(f"error: {', '.join(error_message.args)}")
+    if path:
+        for updated_structure in handle_directory_path(structure,
+                                                       re.sub('^\\./', f'{root_directory}/', path).split('/'),
+                                                       ['.'] if '/' in path else []):
+            structure = updated_structure
+    else:
+        for updated_structure in handle_directory_path(structure, root_directory.split('/')):
+            structure = updated_structure
+
+    if not is_path_found:
+        print(f"error: can not access '{path}': No such file or directory")
         sys.exit()
-if time_sorting_flag:
-    command_object.command_t()
-if reverse_flag:
-    command_object.command_r()
-print(command_object.output_generated_command(all_file_flag, list_info_flag, human_size_flag))
+
+    command_object = UnixCommand(structure)  # Creating Unix Command Objects
+
+    if filter_type:
+        try:
+            command_object.command_filter(filter_name=filter_type)
+        except Exception as error_message:
+            print(f"error: {', '.join(error_message.args)}")
+            sys.exit()
+    if time_sorting_flag:
+        command_object.command_t()
+    if reverse_flag:
+        command_object.command_r()
+    print(command_object.output_generated_command(all_file_flag, list_info_flag, human_size_flag))
